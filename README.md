e6d1f6b3708b2e0f7825f640edf388d67dbd4606
Emmanuel Frécon <efrecon@gmail.com>
2021-11-30 16:58:37 +0100

    Add first version

--- README.md
+++ README.md
@@ -1,2 +1,161 @@
 # docker-compose-build
-Supplements/Replaces `docker compose build` with `docker build` on steroids
+
+Using `docker-compose` in dev, but a more complex orchestrator in production?
+This script considers the `docker-compose.yml` file as the single point of truth
+for build information and will supplement/replace `docker compose build` with
+`docker build` in order to:
+
++ Push the resulting image(s) to their registries.
++ Replace the registry settings from the compose file with another one, if
+  necessary.
++ Retag, or add tags to the images that will be built and/or pushed.
+
+This script has sane defaults. If `docker-compose` is installed, its default
+behaviour is to build all or some of the services that are pointed at by the
+`docker-compose.yml` file in the current directory. When operations such as
+pushing, retagging or changing the destination registry are requested, the
+script will actively read the content of the `docker-compose.yml` and pick the
+necessary information from there. At the time of writing, this will work best
+when the `services` section is last in the file.
+
+The result of this script is a list of the Docker images that were built (or
+[pushed](#flag--p-and-build_push-variable), depending) on `stdout`, one per
+line. This is to facilitate automation. All other output (logging, output of
+`docker` or `docker-compose`, etc.) is redirected to `stderr`.
+
+## Example
+
+### Compose Example
+
+All examples are based on the following compose skeleton:
+
+```yaml
+version: "2.2"
+
+services:
+  myservice:
+  build:
+    context: .
+    dockerfile: Dockerfile
+  image: myregistry.mydomain.com/myimage
+```
+
+### Pure Relay
+
+When `docker-compose` is installed, the following command would simply relay
+calling the `build` sub-command of `docker-compose`. When `docker-compose` is
+not installed (but `docker` is), the script will automatically switch to
+building with the `docker` CLI client. As the default is to read information
+from the `docker-compose.yml` file, the resulting image will be as if it had
+been created by running `docker-compose build`.
+
+```shell
+./build.sh
+```
+
+### Building with Docker
+
+When run with the following command, the script will build using the `docker`
+CLI client. It will automatically pick `buildx` or old-style `build`, in that
+order, and depending on your local installation. The behaviour is the same as
+when running with `docker-compose`: only images that have a `build` context will
+be built.
+
+```shell
+./build.sh -b auto
+```
+
+### Adding a Tag
+
+When run with the following command, the script will automatically add the tag
+`test` to the image that is being built. Adding a tag will automatically depart
+from the default `docker-compose`-based build and use the installed `docker`
+instead.
+
+```shell
+./build.sh -t test
+```
+
+## Command-Line and Environment Configuration
+
+The script accepts a number of command-line options and flags. Its behaviour can
+also be changed through a number of environment variables, all starting with the
+`BUILD_` prefix. Command-line options and flags always have precedence over
+variables. All further arguments to the script will be blindly passed to
+`docker-compose` or `docker` when building. To ensure correctness, you can
+separate the command-line options and flags, from the arguments using a
+double-dash, i.e. `--`.
+
+### Flag `-h`
+
+Provide quick usage summary at the terminal and exit.
+
+### Flag `-v` and `BUILD_VERBOSE` Variable
+
+When the flag is given or the variable set to `1`, the script will describe the
+various steps that it performs more verbosedly on the stderr.
+
+### Flag `-p` and `BUILD_PUSH` Variable
+
+When the flag is given or the variable set to `1`, the images specified by the
+compose file will also be pushed to their respective registries, once building
+has finished. Your local `docker` client must have enough credentials to access
+the remote registry.
+
+### Option `-f` and `BUILD_COMPOSE` Variable
+
+Specifies the location of the compose file to use. The default is to look for a
+file called `docker-compose.yml` first in the current directory, then in the
+same directory as the script is located at.
+
+### Option `-b` and `BUILD_BUILDER` Variable
+
+Specifies the builder to use, can be one of:
+
++ `compose` (the default): will try hard to use compose, but will revert to the
+  `auto` builder (see below) when `docker-compose` is not installed.
++ `auto`: will pick the best of the new `buildx` or old-style `docker build`,
+  depending on which is available, and in that order, i.e. `buildx` preferred.
++ `buildx`: will use the new `buildx` for building. This requires the `buildx`
+  Docker plugin to be available and properly installed.
++ `docker` or `build`: will use the old-style `docker build` command.
+
+### Option `-s` and `BUILD_SERVICES` Variable
+
+Specifies the space separated list of services to build. These services need to
+exist in the compose file. When empty, the default, the script will default to
+building/pushing all the services specified in the compose file.
+
+### Option `-t` and `BUILD_TAGS` Variable
+
+Specifies the space separated list of tags to give to the images that will be
+built/pushed. When building, the image with the first tag in the list will be
+built, while images with the other tags will be tagged with the first image as
+the source. The default is to not specified any tag, in which case the tag from
+the compose file will be picked up and used, if any.
+
+### Option `-r` and `BUILD_REGISTRY` Variable
+
+Specified an alternative registry to use instead of the one specified as part of
+the compose file. When no registry is given, the default, the registry will be
+the one from the compose file.
+
+### Option `-a` and `BUILD_AGE` Variable
+
+Specifies the maximum age of the image since creation to decide whether it
+should be pushed or not. This is a safety measure to avoid pushing junk images
+that would have been built in the past. The default of `1200` seconds should
+work in most cases, but any negative value will turn this check off, meaning
+that all relevant images will be pushed, disregarding their age.
+
+### `BUILD_COMPOSE_BIN` Variable
+
+Specifies how to run `docker-compose`, which also is the default value.
+
+### `BUILD_DOCKER_BIN` Variable
+
+Specifies how to run `docker`, which also is the default value. This makes it,
+in theory, possible to use alternatives such as [podman] or [nerdctl].
+
+  [podman]: https://github.com/containers/podman
+  [nerdctl]: https://github.com/containerd/nerdctl
---
+++ build.sh
@@ -0,0 +1,459 @@
+#!/usr/bin/env sh
+
+# If editing from Windows. Choose LF as line-ending
+
+# This script builds (and push) Docker images that have build information in a
+# docker-compose file.
+
+set -eu
+
+ROOT_DIR=${ROOT_DIR:-"$( cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P )"}
+
+pathfind() {
+  printf %s\\n "$1"|sed 's/:/\n/g'|grep -vE '^$'|while IFS= read -r dir; do
+    find "$dir" -mindepth 1 -maxdepth 1 -name "$2" 2>/dev/null
+  done | head -n 1
+}
+
+# The location of the compose file. This file contains information about the
+# images to generate.
+BUILD_COMPOSE=${BUILD_COMPOSE:-$(pathfind "$(pwd):${ROOT_DIR%/}" docker-compose.yml)}
+
+# The list of services to build/push for. When empty, this will be a good guess
+# of all services (the guess is perfect when building with compose).
+BUILD_SERVICES=${BUILD_SERVICES:-}
+
+# What builder to use. This can be compose, docker, build, buildx or auto. build
+# and docker are just aliases for the same. When running with compose, the
+# script will run docker-compose build. In all other cases, the script will
+# analyse the compose file to detect services, images, contexts and dockerfile
+# and pass proper arguments to docker build (or buildx). auto will resolve to
+# the best available of buildx or docker.
+BUILD_BUILDER=${BUILD_BUILDER:-"compose"}
+
+# Should we also push the generated images to the registry.
+BUILD_PUSH=${BUILD_PUSH:-0}
+
+# List of tags to give images (does not work with compose)
+BUILD_TAGS=${BUILD_TAGS:-}
+
+# Secondary Docker registry to build for and push to (does not work with
+# compose)
+BUILD_REGISTRY=${BUILD_REGISTRY:-}
+
+# Age of the image for pushes to be allowed (this avoid pushing old images, just
+# a failsafe measure)
+BUILD_AGE=${BUILD_AGE:-1200}
+
+# Print out more information on stderr
+BUILD_VERBOSE=${BUILD_VERBOSE:-0}
+
+# How to run the docker and docker-compose clients
+BUILD_DOCKER_BIN=${BUILD_DOCKER_BIN:-"docker"}
+BUILD_COMPOSE_BIN=${BUILD_COMPOSE_BIN:-"docker-compose"}
+
+usage() {
+  # This uses the comments behind the options to show the help. Not extremly
+  # correct, but effective and simple.
+  echo "$0 builds (and push) Docker images. Arguments passed to compose/build. Usage:" && \
+    grep "[[:space:]].)\ #" "$0" |
+    sed 's/#//' |
+    sed -r 's/([a-z])\)/-\1/'
+  exit "${1:-0}"
+}
+
+while getopts "f:b:s:t:r:a:pvh?-" opt; do
+  case "$opt" in
+    f) # Path to docker compose file to use. Defaults to docker-compose.yml in current directory or same directory as script.
+      BUILD_COMPOSE=$OPTARG;;
+    p) # Should the built images be pushed to the registry?
+      BUILD_PUSH=1;;
+    b) # Builder to use: compose, auto, docker or buildx
+      BUILD_BUILDER=$OPTARG;;
+    s) # List of (compose) services to build/push, defaults to all that have a build context
+      BUILD_SERVICES=$OPTARG;;
+    t) # Tag to give to images, forces use of auto for builder when specified.
+      BUILD_TAGS=$OPTARG;;
+    r) # Docker registry+leading path to push to, instead of the one from Docker compose file. Forces use of auto for builder when specified
+      BUILD_REGISTRY=$OPTARG;;
+    a) # Maximum age of the image when pushing, older will be discarded. Negative to turn off.
+      BUILD_AGE=$OPTARG;;
+    v) # More verbosity on stderr
+      BUILD_VERBOSE=1;;
+    h | \?) # Print this help and exit
+      usage;;
+    -)
+      break;;
+    *)
+      usage 1;;
+  esac
+done
+shift $((OPTIND-1))
+
+# Poor man's logging
+_message() { printf '[%s] [%s] %s\n' "$(date +'%Y%m%d-%H%M%S')" "${1:-DBG}" "$2" >&2; }
+verbose() {
+  if [ "$BUILD_VERBOSE" = "1" ]; then
+    _message "NFO" "$1"
+  fi
+}
+warn() { _message "WRN" "$1"; }
+
+
+# Without a 2nd argument, this function will return the entire definition of the
+# service which name is passed as a first argument. When a 2nd argument is
+# given, it should be an integer and only the lines at this exact identation
+# level passed will be returned
+service() {
+  if [ -z "${2:-}" ]; then
+    # Find the line (within the services section) where the service definition
+    # for $1 starts.
+    svc_line=$( tail +"$svc_section" "$BUILD_COMPOSE" |
+                grep -En "^${indent}${1}:" |
+                cut -d: -f1 )
+    # Find the end line, or rather the line at which the next service definition
+    # starts. This might be empty, when the service is last in the file
+    end_line=$( tail +"$svc_section" "$BUILD_COMPOSE" |
+                tail +"$svc_line" |
+                grep -En "^${indent}[a-zA-Z0-9].*:" |
+                tail +2 |
+                head -1 |
+                cut -d: -f1 )
+    if [ -z "$end_line" ]; then
+      # No end_line, service was last, just tail
+      tail +"$svc_section" "$BUILD_COMPOSE" |
+          tail +"$svc_line"
+    else
+      # There was an end_line, decrement by 1 and print out this sub-section
+      # only.
+      end_line=$(( end_line - 1))
+      tail +"$svc_section" "$BUILD_COMPOSE" |
+          tail +"$svc_line" |
+          head -"$end_line"
+    fi
+  else
+    # When an identation level is specified, do as if none was specify to get
+    # the entire service definition, then isolate the keys. Arrange to have the
+    # dash in the grep, in case an array is at this identation level.
+    lvl=$(printf "%${2}s" "" | sed "s/ /${indent}/g"); # repeats indent $2 times
+    service "$1" | grep -E "^${lvl}[a-z-]"
+  fi
+}
+
+
+# Provided identation and the service section have been properly detected in the
+# compose file, this function will look for the value of the keyword $3, in the
+# service description of $1. The keyword should be at indentation level $2.
+valueof() {
+  service "$1" "$2" |
+    grep "$3" |
+    head -n 1 |
+    sed -E "s/^[[:space:]]+${3}:[[:space:]]+(.*)/\\1/" |
+    sed -E -e 's/^"//' -e 's/"$//' -e "s/^'//" -e "s/'$//"
+}
+
+
+# Return the list of services
+services() {
+  tail +"$svc_section" "$BUILD_COMPOSE" |
+    grep -E "^${indent}[a-zA-Z0-9].*:" |
+    sed -E -e 's/^\s+//g' -e 's/:$//g'
+}
+
+
+# Change registry of the image passed as $1
+reroot() {
+  # shellcheck disable=SC3043
+  local img ctag imgname || true
+
+  # Extract the fully-qualified name of the image out of the first argument.
+  img=$(printf %s\\n "$1"|grep -Eo '[a-zA-Z0-9.]+(:[0-9]+)?(/[a-zA-Z0-9_.-]+){1,3}')
+  # Extract the tag. The regular expression works by refusing the : or the / in
+  # the tag, the / is to handle the case where the registry is a DNS together
+  # with a port (led by a colon, but there will be a slash after that colon).
+  # Note that the tag WILL include the leading colon sign; this is on purpose.
+  # The entire extraction goes through || true, because some images have no tag.
+  ctag=$(printf %s\\n "$1"|grep -Eo ':[^:^/]+$' || true)
+  # Extract the name of the image, i.e. everything after the LAST slash.
+  imgname=$(printf %s\\n "$img" | awk -F / '{print $NF}')
+  # Reroot under the registry passed as a second argument (or the default one
+  # from BUILD_REGISTRY), keeping the same tag.
+  printf %s/%s%s\\n "${2:-${BUILD_REGISTRY%/}}" "$imgname" "$ctag"
+}
+
+
+# Return the image to build/push for the service passed as first argument. When
+# a second argument is specified, it is the tag to give to the image. When a
+# different registry has been specified globally, the current registry of the
+# image will be replace by the global registry.
+image() {
+  # shellcheck disable=SC3043
+  local image || true
+
+  image=$(valueof "$1" 2 "image")
+
+  # Does the image contain a reference to a shell variable of some sort?
+  # shellcheck disable=SC2016 # We **want** to detect the $ sign!!
+  if printf %s\\n "$image" | grep -qE '\${?[A-Z_]+'; then
+    # When it does, we resolve the value of the variable through evaluation
+    # an output of the image content. This is ugly...
+    image=$(eval echo "$image")
+  fi
+
+  # If a specific build tag is specified, either we replace the existing tag
+  # of the image, or we just add it to the image name.
+  if [ -n "${2:-}" ]; then
+    if printf %s\\n "$image" | grep -qE ':[^:^/]+$'; then
+      image=$(printf %s\\n "$image" | sed -E 's|:[^:^/]+$|:'"${2#:}"'|')
+    else
+      image="${image}:${2#:}"
+    fi
+  fi
+
+  if [ -z "$BUILD_REGISTRY" ]; then
+    printf %s\\n "$image"
+  else
+    printf %s\\n "$(reroot "$image" "$BUILD_REGISTRY")"
+  fi
+}
+
+
+# Build the image for the service passed as a first parameter, forcing its tag
+# to be the value of the second parameter.  When the second argument is empty,
+# the original tag from the image will be kept. All other arguments will be
+# blindly passed to the docker build command.
+docker_build() {
+  # Pick image name to build, at proper tag
+  if [ -z "${2:-}" ]; then
+    verbose "Building image from service $1"
+    image=$(image "$1")
+  else
+    verbose "Building image from service $1 (tag: $2)"
+    image=$(image "$1" "${2}")
+  fi
+
+  # Pick context and Dockerfile location from the service description, default
+  # to the same as docker-compose defaults.
+  context=$(valueof "$1" 3 "context")
+  if [ -z "$context" ]; then
+    context=$(dirname "$BUILD_COMPOSE")
+  fi
+  dockerfile=$(valueof "$1" 3 "dockerfile")
+  if [ -z "$dockerfile" ]; then
+    dockerfile="Dockerfile"
+  fi
+
+  # Done with the two first arguments, everything else will be passed further to
+  # docker build.
+  shift 2
+
+  # Decide upon command to use for building, e.g. old-style docker build or
+  # new buildx with BuildKit.
+  if [ "$BUILD_BUILDER" = "docker" ]; then
+    buildercmd="${BUILD_DOCKER_BIN} build"
+  else
+    buildercmd="${BUILD_DOCKER_BIN} buildx build --load"
+  fi
+
+  # Perform build command, we do this in a sub-shell to be able to temporarily
+  # change directory.
+  ( cd "${context}" \
+    && $buildercmd -t "$image" -f "$dockerfile" "$@" . ) 1>&2
+
+  # When we won't have to push, the list of images printed on the stdout is the
+  # list of built images. So print the name of the image out.
+  if [ "$BUILD_PUSH" = "0" ]; then
+    printf %s\\n "$image"
+  fi
+}
+
+# Push the image for the service passed as a first parameter, forcing its tag
+# to be the value of the second parameter.  When the second argument is empty,
+# the original tag from the image will be kept.
+image_push() {
+  # Pick image name to push, at proper tag
+  if [ -z "${2:-}" ]; then
+    verbose "Conditionally pushing image from service $1"
+    image=$(image "$1")
+  else
+    verbose "Conditionally pushing image from service $1 (tag: $2)"
+    image=$(image "$1" "${2}")
+  fi
+
+  if service "$1" 2 | grep -q build \
+      && ${BUILD_DOCKER_BIN} image inspect "$image" >/dev/null 2>&1; then
+    # Extract the ISO8601 when the image was last created
+    isodate=$(  ${BUILD_DOCKER_BIN} image inspect "$image" |
+                grep Created |
+                sed -E 's/\s+"Created"\s*:\s*"([^"]+)".*/\1/' )
+    # Convert the ISO8601 date to the number of seconds since the epoch. This
+    # removes the T and remove the milli/microseconds from the date string to
+    # arrange for date -d to be able to parse. Note: On busybox, this will
+    # loose seconds, but this isn't a big deal for us.
+    tstamp=$(date -d "$(printf %s\\n "$isodate"  | sed -E -e 's/([0-9])T([0-9])/\1 \2/' -e 's/\.[0-9]+/ /')" +%s)
+    # Compute how old the image is and push only if it is young, i.e. we've
+    # just built it.
+    now=$(date +%s)
+    age=$(( now - tstamp ))
+    if [ "$age" -lt "$BUILD_AGE" ] || [ "$BUILD_AGE" -le "0" ]; then
+      verbose "Pushing $image to Docker registry"
+      ${BUILD_DOCKER_BIN} push "$image" 1>&2
+      # When we have to push, the list of images printed on the stdout is the
+      # list of pushed images. So print the name of the image out.
+      if [ "$BUILD_PUSH" = "1" ]; then
+        printf %s\\n "$image"
+      fi
+    else
+      warn "$image is too old, last created: $isodate"
+    fi
+  fi
+}
+
+
+# Cannot continue without a docker-compose file
+if ! [ -f "$BUILD_COMPOSE" ]; then
+  warn "Cannot find compose file at ${BUILD_COMPOSE}!"
+  exit 1
+fi
+
+# Change builder when a tag or registry are specified, or when docker-compose
+# isn't installed at all.
+if [ "$BUILD_BUILDER" = "compose" ]; then
+  if ! command -v "${BUILD_COMPOSE_BIN}" >/dev/null 2>&1; then
+    warn "Using auto builder instead, ${BUILD_COMPOSE_BIN} not installed or accessible from PATH"
+    BUILD_BUILDER="auto"
+  fi
+  if [ -n "$BUILD_TAGS" ]; then
+    warn "Using auto builder instead, as a different tag is specified"
+    BUILD_BUILDER="auto"
+  fi
+  if [ -n "$BUILD_REGISTRY" ]; then
+    warn "Using auto builder instead, as a different registry is specified"
+    BUILD_BUILDER="auto"
+  fi
+fi
+
+# Test value of builder, and presence of docker CLI
+case "$BUILD_BUILDER" in
+  compose ) ;;
+  auto | docker | build* )
+    if ! command -v "${BUILD_DOCKER_BIN}" >/dev/null 2>&1; then
+      warn "${BUILD_DOCKER_BIN} not installed or accessible from PATH"
+      exit 1
+    fi
+    ;;
+  * )
+    warn "$BUILD_BUILDER is not a known builder type!"
+    exit 1
+    ;;
+esac
+
+# Resolve auto to the best available of docker buildx or old-style docker.
+if [ "$BUILD_BUILDER" = "auto" ]; then
+  if ${BUILD_DOCKER_BIN} buildx >/dev/null; then
+    verbose "Using buildx for building image(s)"
+    BUILD_BUILDER=buildx
+  else
+    verbose "Using old-style docker build"
+    BUILD_BUILDER=docker
+  fi
+fi
+
+# Guess the characters used for indentation in the compose file and where the
+# services section starts in the file.
+indent=$(grep -E '^\s+' "$BUILD_COMPOSE" |head -n 1|sed -E 's/^(\s+).*/\1/')
+svc_section=$(grep -En '^services' "$BUILD_COMPOSE"|cut -d: -f1)
+
+if [ -z "$BUILD_SERVICES" ]; then
+  verbose "Will build all services out of $BUILD_COMPOSE"
+else
+  verbose "Will build following services from $BUILD_COMPOSE: $BUILD_SERVICES"
+fi
+
+# Build image
+case "$BUILD_BUILDER" in
+  compose)
+    # We will be running with compose, so we need to analyse its output in order
+    # to be able to print out the name of the images that are created by the
+    # build commands. For this to happen, while still catching errors, we'll
+    # make a fifo that we will dump (to stderr) which capturing and analysing
+    # the result of compose in real-time.
+    fifo=$(mktemp -tu compose-XXXXX.log)
+    mkfifo "$fifo"
+    cat "$fifo" >&2 &
+    # When running with compose, we can rely on compose to analyse the YAML
+    # file. So we just run docker-compose build, for all or just the services
+    # specified.
+    if [ -z "$BUILD_SERVICES" ]; then
+      image=$(  ${BUILD_COMPOSE_BIN} -f "$BUILD_COMPOSE" build "$@" |
+                tee -a "$fifo" |
+                grep "Successfully tagged" |
+                sed -E 's/^Successfully tagged\s+(.*)/\1/')
+      if [ "$BUILD_PUSH" = "0" ]; then
+        printf %s\\n "$image"
+      fi
+    else
+      for svc in $BUILD_SERVICES; do
+        image=$(  ${BUILD_COMPOSE_BIN} -f "$BUILD_COMPOSE" build "$@" -- "$svc" |
+                  tee -a "$fifo" |
+                  grep "Successfully tagged" |
+                  sed -E 's/^Successfully tagged\s+(.*)/\1/')
+        if [ "$BUILD_PUSH" = "0" ]; then
+          printf %s\\n "$image"
+        fi
+      done
+    fi
+    rm -f "$fifo"
+    ;;
+  docker | build*)
+    # Detect list of services to build
+    if [ -z "$BUILD_SERVICES" ]; then
+      BUILD_SERVICES=$(services)
+    fi
+
+    for svc in $BUILD_SERVICES; do
+      if service "$svc" 2 | grep -q build; then
+        if [ -z "$BUILD_TAGS" ]; then
+          docker_build "$svc" "" "$@"
+        else
+          main=
+          for tag in $BUILD_TAGS; do
+            if [ -z "$main" ]; then
+              docker_build "$svc" "$tag" "$@"
+              main=$tag
+            else
+              verbose "Re-tagging image from service $svc, tag: $tag"
+              $BUILD_DOCKER_BIN image tag \
+                "$(image "$svc" "$main")" \
+                "$(image "$svc" "$tag")" 1>&2
+
+              if [ "$BUILD_PUSH" = "0" ]; then
+                printf %s\\n "$(image "$svc" "$tag")"
+              fi
+            fi
+          done
+        fi
+      else
+        verbose "Service $svc has no build information"
+      fi
+    done
+    ;;
+esac
+
+# Push if requested
+if [ "$BUILD_PUSH" = "1" ]; then
+  # Detect list of services to build
+  if [ -z "$BUILD_SERVICES" ]; then
+    BUILD_SERVICES=$(services)
+  fi
+
+  for svc in $BUILD_SERVICES; do
+    if [ -z "$BUILD_TAGS" ]; then
+      image_push "$svc"
+    else
+      for tag in $BUILD_TAGS; do
+        image_push "$svc" "$tag"
+      done
+    fi
+  done
+fi
