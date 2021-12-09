1159a62f9b40ad7e0b8753ac63d59b945712d44e
Emmanuel Frécon <efrecon@gmail.com>
2021-12-09 11:14:40 +0100

    Prepare for release

--- build.sh
+++ build.sh
@@ -25,7 +25,7 @@ pathfind() {
 }
 
 # Version of script, this should be increased for each new release of the script
-BUILD_VERSION=1.5.0-beta
+BUILD_VERSION=1.5.0
 
 # The location of the compose file. This file contains information about the
 # images to generate.
